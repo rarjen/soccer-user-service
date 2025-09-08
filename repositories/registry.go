cb823cee5de137844d3d7a5b75fb9528f9501d0a
Otniel Kevin Abiel <rarjen57@gmail.com>
2025-09-08 10:16:14 +0700

    Refactor registry: Add IUserRepositoryRegistry interface and implement GetUser method

--- repositories/registry.go
+++ repositories/registry.go
@@ -1,1 +1,19 @@
 package repositories
+
+import (
+	repositories "user-service/repositories/user"
+
+	"gorm.io/gorm"
+)
+
+type Registry struct {
+	db *gorm.DB
+}
+
+type IUserRepositoryRegistry interface {
+	GetUser() repositories.IUserRepository
+}
+
+func (r *Registry) GetUser() repositories.IUserRepository {
+	return repositories.NewUserRepository(r.db)
+}
